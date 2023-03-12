1c6f9fcf482ee85fd924f4a36b4c4a1bcc88e0fe
richardconrardy <rc@lnet.ch>
2023-03-12 09:58:09 +0100

    alles hochladen

---
+++ content/slides/_index.md
@@ -0,0 +1,9 @@
++++
+title = "Präsentationen"
+date = "2017-04-24T18:36:24+02:00"
++++
+
+# Slideliste
+
+
+* [Slideshow]({{%relref "slides/myslide.md" %}})
---
+++ public/de/index.search.js
@@ -0,0 +1,23 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cap'n Hugo Relearrrn Theme",
+    "uri": "/de/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/de/categories.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/de/tags.html"
+  }
+]
